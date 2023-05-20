cd1289d5f014f665f52b76f9344be704ac9fa99d
ygz <yagiz.gnc00@gmail.com>
2023-05-20 15:48:07 +0300

    Add project files.

---
+++ MVC_MyProject/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MVC_MyProject.Models;
+using MyECommerce.Entity.Entity;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+
+namespace MVC_MyProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public HomeController(ILogger<HomeController> logger, SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+        {
+            _logger = logger;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC_MyProject/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MVC_MyProject.Models.ViewModels;
+using MyECommerce.DAL.Context;
+using MyECommerce.Entity.Entity;
+
+namespace MVC_MyProject.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly MyECommerceContext _myECommerceContext;
+
+        public UserController(UserManager<AppUser> userManager,MyECommerceContext myECommerceContext)
+        {
+            _userManager = userManager;
+            _myECommerceContext = myECommerceContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(RegisterVM registerVM)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user=new AppUser();
+
+                user.UserName = registerVM.Username;
+                user.Email = registerVM.Email;
+
+                var result = await _userManager.CreateAsync(user, registerVM.ConfirmPassword);
+
+                if (result.Succeeded) 
+                {   
+                    return RedirectToAction("Index","Home");
+                }
+                else
+                {
+                    return View(registerVM);
+                }
+
+            }
+            else { return View(registerVM); }
+        }
+    }
+}
